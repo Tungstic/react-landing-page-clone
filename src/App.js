ee98acd18f224381945d3fae8b8942de2a7dfc27
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-09 20:28:33 +0200

    Install fonts

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import { ReactComponent as Logo } from './images/logo.svg';
 export default function App() {
   return (
     <>
-      <header>
+      <header className={styles.logoAndNavBar}>
         <a href="/#">
           <Logo />
         </a>
-        heading here
+        <nav className={`${styles.navigationBar} ${styles.mainText}`}>
+          <ul>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Reviews</li>
+            <li>Log In</li>
+            <li>Get Started</li>
+          </ul>
+        </nav>
       </header>
-      <main>9 sections</main>
+      <main>
+        <div>
+          <h1 className={styles.headingFont}>
+            Cut back on your drinking with no pressure to quit.
+          </h1>
+          <p className={styles.mainText}>
+            146 million U.S. adults drink alcohol, and 47% want to cut back. If
+            you're one of them, we have the tools to help, and we'll never judge
+            you.
+          </p>
+        </div>
+      </main>
       <footer>footer with underfooter for copyright</footer>
     </>
   );
