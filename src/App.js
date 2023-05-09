100943db4ac85d0013ccea5f096a2774a2d0f471
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-09 19:26:36 +0200

    Import logo.svg as a component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-export default function App() {}
+import styles from './App.module.scss';
+import { ReactComponent as Logo } from './images/logo.svg';
+
+export default function App() {
+  return (
+    <>
+      <header>
+        <a href="/#">
+          <Logo />
+        </a>
+        heading here
+      </header>
+      <main>9 sections</main>
+      <footer>footer with underfooter for copyright</footer>
+    </>
+  );
+}
