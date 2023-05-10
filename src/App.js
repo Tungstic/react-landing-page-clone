cbf8f463029bcf55c4037795ec57a89a43f3dd78
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-05-10 17:35:42 +0200

    Add 6 SVGs to the second section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import styles from './App.module.scss';
+import { ReactComponent as GridPicOne } from './images/grid1.svg';
+import { ReactComponent as GridPicTwo } from './images/grid2.svg';
+import { ReactComponent as GridPicThree } from './images/grid3.svg';
+import { ReactComponent as GridPicFour } from './images/grid4.svg';
+import { ReactComponent as GridPicFive } from './images/grid5.svg';
+import { ReactComponent as GridPicSix } from './images/grid6.svg';
 import { ReactComponent as Logo } from './images/logo.svg';
 
 export default function App() {
@@ -8,7 +14,7 @@ export default function App() {
         <a href="/#">
           <Logo />
         </a>
-        <nav className={`${styles.navigationBar} ${styles.mainText}`}>
+        <nav className={styles.navigationBar}>
           <ul>
             <li>About</li>
             <li>Blog</li>
@@ -36,13 +42,44 @@ export default function App() {
           />
         </div>
 
-        <div className={styles.secondSection}>
+        <div className={`${styles.secondSection} ${styles.container}`}>
           <h2>Small changes, big impact</h2>
           <p>
             Sunnyside provides a simple but structured approach to help you
             drink more mindfully. You'll feel the difference.
           </p>
-          <div>Grid of divs with svg, heading and text</div>
+          <div className={styles.gridOfSix}>
+            <div>
+              <GridPicOne />
+              <h3>More Energy</h3>
+              <p>Greet the day with increased energy and focus.</p>
+            </div>
+            <div>
+              <GridPicTwo />
+              <h3>Improved Wellness</h3>
+              <p>Feel your best, inside and out.</p>
+            </div>
+            <div>
+              <GridPicThree />
+              <h3>Better Sleep</h3>
+              <p>Enjoy more restful sleep and brighter mornings.</p>
+            </div>
+            <div>
+              <GridPicFour />
+              <h3>Healthier Diet</h3>
+              <p>Cut the empty sugar from those extra drinks.</p>
+            </div>
+            <div>
+              <GridPicFive />
+              <h3>Save Money</h3>
+              <p>Spend less on drinks while still enjoying yourself</p>
+            </div>
+            <div>
+              <GridPicSix />
+              <h3>Have More Fun</h3>
+              <p>Make the drinks you have more enjoyable.</p>
+            </div>
+          </div>
         </div>
       </main>
       <footer>footer with underfooter for copyright</footer>
